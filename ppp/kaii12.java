11d5605a09af743dbc786532e804af01dcc47cdc
pppkaii <kailapiad@gmail.com>
2025-12-07 10:33:42 +0800

    Add frequency check for target number in array

--- ppp/kaii12.java
+++ ppp/kaii12.java
@@ -1,1 +1,20 @@
-
+public class kaii12
+{
+    public static void main(String[] args){
+        int[] numbers = {5, 2, 5, 7, 5, 9};
+        int target = 5;
+        int count = 0;
+        
+        for (int num : numbers) {
+            if (num == target) {
+            count ++;
+            }
+        }
+        
+       if (count >= 3) {
+           System.out.println(target + " appears at least 3 times.");
+        } else {
+        System.out.println(target + " does NOT appear 3 times.");
+       }
+    }
+}
