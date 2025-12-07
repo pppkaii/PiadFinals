7877dc766d95f57bf98ec31bbe5cf2fa069a6f42
pppkaii <kailapiad@gmail.com>
2025-12-07 10:47:44 +0800

    Create kaii14 class to print numbers in reverse
    Added class kaii14 with a main method to print numbers in reverse order.

--- ppp/kaii14.java
+++ ppp/kaii14.java
@@ -1,1 +1,17 @@
 
+/**
+ * Write a description of class kaii14 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class kaii14
+{
+    public static void main(String[] args){
+        int[] numbers = {10, 20, 30, 40, 50};
+        
+        for (int i = numbers.length - 1; i >= 0; i--) {
+            System.out.println(numbers[i] + " ");
+        }
+    }
+}
