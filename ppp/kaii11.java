328e698ace11e69c8b990258cb8e9a6d33f4503c
pppkaii <kailapiad@gmail.com>
2025-12-07 10:10:28 +0800

    Add loop to print every second number
    Prints every second element from the numbers array.

--- ppp/kaii11.java
+++ ppp/kaii11.java
@@ -1,1 +1,11 @@
+public class kaii11
+{
+    public static void main(String[] args){
+        int[] numbers = {10, 20, 30, 40, 50, 60};
+        
+        for (int i = 0; i < numbers.length; i +=2) {
+        System.out.println("Index " + i + ": " + numbers[i]);
+        }
 
+    }
+}
