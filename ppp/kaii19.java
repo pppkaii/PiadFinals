01ab35be765c050bee6a9446a2d35656531fc351
pppkaii <kailapiad@gmail.com>
2025-12-07 11:47:16 +0800

    Create kaii19 class to display student grades
    Added a class with a main method to display student grades.

--- ppp/kaii19.java
+++ ppp/kaii19.java
@@ -1,1 +1,18 @@
 
+/**
+ * Write a description of class kaii19 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class kaii19
+{
+    public static void main(String [] args){
+        String[] students = {"Alice", "Bob", "Charlie", "David"};
+        int[] grades = {85, 92, 78, 90};
+        
+        for (int i = 0; i < students.length; i++) {
+             System.out.println(students[i] + " has grade: " + grades[i]);
+        }
+}
+}
