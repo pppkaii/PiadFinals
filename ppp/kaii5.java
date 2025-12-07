6f6288b18cdf7aa0e32d0337e1c6d82703ac778a
pppkaii <kailapiad@gmail.com>
2025-12-07 09:24:51 +0800

    Create kaii5 class with main method for array search
    Added a class definition and a main method to search for an element in an array.

--- ppp/kaii5.java
+++ ppp/kaii5.java
@@ -1,1 +1,19 @@
-
+/**
+ * Write a description of class kaii5 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class kaii5{
+    public static void main(String [] args){
+        int[] arr = {3, 8, 1, 9, 5};
+        int find = 9;
+        
+        for(int i = 0; i <arr.length; i++){
+        if (arr[i] == find){
+            System.out.println("Found at index" + i);
+            break;
+    }        
+    }
+    }
+}
