ada0988c0c10f93fe1813fef12a48f3ccc3f5ff3
pppkaii <kailapiad@gmail.com>
2025-12-07 10:02:17 +0800

    Implement logic to find first even number index
    Fix logic for finding the first even number in an array.

--- ppp/kaii10.java
+++ ppp/kaii10.java
@@ -1,1 +1,20 @@
-
+public class kaii10
+{
+   public static void main(String[] args){
+       int [] numbers = {5, 7, 9, 12, 15, 20};
+       int index = 1;
+       
+       for (int i = 0; i < numbers.length; i++) {
+           if (numbers[i] % 2 == 0) {
+               index = i;
+               break;
+       }
+       
+           if (index != -1) {
+               System.out.println("First even number found at index: " + index);
+            } else {
+                System.out.println("No even number bfound.");
+       }
+}
+}
+}
