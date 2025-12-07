47678add8ddf7b15e309e283c57f9191e7a62216
pppkaii <kailapiad@gmail.com>
2025-12-07 11:52:36 +0800

    Create kaii20 class to filter even numbers
    Added a class that prints even numbers greater than 20.

--- ppp/kaii20.java
+++ ppp/kaii20.java
@@ -1,1 +1,20 @@
 
+/**
+ * Write a description of class kaii20 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class kaii20
+{
+    public static void main(String [] args){
+        int[] numbers = {10, 25, 30, 17, 40, 21, 50};
+        
+        System.out.println("Even numbers greater than 20:");
+        for (int num : numbers) {
+            if (num > 20 && num % 2 == 0) {
+                System.out.println(num);
+            }
+        }
+}
+}
