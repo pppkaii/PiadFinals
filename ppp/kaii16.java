fd6875831ea68b415663f7dc2ff2d19349d79a7b
pppkaii <kailapiad@gmail.com>
2025-12-07 11:08:44 +0800

    Implement digit counting in kaii16.java
    Added a Java program to count the number of digits in an integer.

--- ppp/kaii16.java
+++ ppp/kaii16.java
@@ -1,1 +1,29 @@
 
+/**
+ * Write a description of class kaii16 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+import java.util.Scanner;
+public class kaii16
+{
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+        int count = 0;
+        int temp = Math.abs(number);
+        
+        if (temp == 0) {
+            count = 1;
+        } else {
+            while (temp > 0) {
+                temp /= 10;
+                count++;
+            }
+        }
+        
+        System.out.println("Number of digits; " + count);
+    }
+}
