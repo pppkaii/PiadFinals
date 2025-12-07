d3d8fe0439b6cfa978dc5a8affb798f4e9f9fbd3
pppkaii <kailapiad@gmail.com>
2025-12-07 10:59:33 +0800

    Update kaii15.java

--- ppp/kaii15.java
+++ ppp/kaii15.java
@@ -1,1 +1,28 @@
 
+/**
+ * Write a description of class kaii15 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+import java.util.Scanner;
+public class kaii15
+{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int[] numbers = new int[5];
+        
+        for (int i = 0; i< 5; i++) {
+            System.out.print("Enter number " + (i+1) + ": ");
+            numbers[i] = sc.nextInt();
+        }
+        
+        
+        System.out.println("Numbers greater than 50:");
+        for (int num : numbers) {
+            if (num > 50) {
+                System.out.println(num);
+            }
+        }
+    }
+}
